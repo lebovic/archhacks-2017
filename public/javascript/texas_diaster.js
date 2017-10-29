6bafcec50463fac23d2bc594d3cf34091e78eb49
Noah Lebovic <noah@lebovic.com>
2017-10-28 21:57:54 -0500

    Actually fetch data from server

--- public/javascript/texas_diaster.js
+++ public/javascript/texas_diaster.js
@@ -1,4 +1,4 @@
-const TEXAS_DIASTER_DATA = {	
+const TEXAS_DIASTER_DATA = {
 	"MIN_ZOOM"   : 11,
 	"CENTER" : { "lat" : 29.757478, "lng" : -95.3514902 },
 	"ORIGIN" : { "lat" : 29.620715, "lng" : -95.5233427 },
@@ -33,50 +33,41 @@ function texas_diaster_init() {
 	// create 2d array to hold summation of data
 	resizeTexasDiasterMap();
 
-	// $.ajax({
- //        headers: { "Accept": "application/json"},
- //        type: 'GET',
- //        url: 'http://241fcd3e.ngrok.io/api/resources',
- //        crossDomain: true,
- //        beforeSend: function(xhr){
- //            xhr.withCredentials = true;
- //      },
- //        success: function(data, textStatus, request){
-            var data = [{"supplies":["food","water"],"lat":29.7361352,"lng":-95.34327030000001,"contact":18476518454},{"supplies":["food","water"],"lat":29.7361352,"lng":-95.34327030000001,"contact":18476518454},{"supplies":["food","medical","rescue"],"lat":29.7429276,"lng":-95.3706247,"contact":16513354751},{"supplies":["food","water"],"lat":29.7492286,"lng":-95.3845977,"contact":16513354751},{"supplies":["medical","rescue"],"lat":29.7988129,"lng":-95.3107455,"contact":16513354751},{"supplies":["food","water"],"lat":29.6343504,"lng":-95.3422324,"contact":16513354751},{"supplies":["rescue"],"lat":29.6328914,"lng":-95.214253,"contact":16513354751},{"supplies":["food","medical"],"lat":29.7428491,"lng":-95.2638144,"contact":16513354751},{"supplies":["water","rescue"],"lat":29.74880649999999,"lng":-95.6104287,"contact":16513354751},{"supplies":["medical"],"lat":29.7910839,"lng":-95.5773172,"contact":16513354751},{"supplies":["rescue"],"lat":29.842126,"lng":-95.46169700000002,"contact":16513354751},{"supplies":["water","rescue"],"lat":29.8639322,"lng":-95.4204573,"contact":16513354751},{"supplies":["medical","rescue"],"lat":29.6722799,"lng":-95.3060788,"contact":16513354751},{"supplies":["food","water","rescue"],"lat":29.6720417,"lng":-95.578096,"contact":16513354751},{"supplies":["food","water"],"lat":29.6598426,"lng":-95.5771139,"contact":16513354751}];
-//.get("http://241fcd3e.ngrok.io/api/resources", function(data) {
-			// data == {"contact": Number,"lat": Number,"lng": Number, supplies":[String] }
-			var x,y;
-			
-			// This is the logic to normalize all the data and group it up
-			for(var i = 0; i < data.length; i++) {
-				// ya, my b on this equation!			
-				x = Math.abs(parseInt(texas_diaster_grid_size * ((data[i].lng - TEXAS_DIASTER_DATA.ORIGIN.lng) / texas_diaster_normalize_w)));
-				y = Math.abs(parseInt(texas_diaster_grid_size * ((data[i].lat - TEXAS_DIASTER_DATA.ORIGIN.lat) / texas_diaster_normalize_h)));
-				
-				if (data[i].supplies.indexOf("rescue") >= 0) {
-					texas_diaster_grid[x][y].rescue++;
-				}
-				if (data[i].supplies.indexOf("food") >= 0) {
-					texas_diaster_grid[x][y].food++;
-				} 
-				if (data[i].supplies.indexOf("medical") >= 0) {
-					texas_diaster_grid[x][y].medical++;
-				} 
-				if (data[i].supplies.indexOf("water") >= 0) {
-					texas_diaster_grid[x][y].water++;
-				}				
-			}
+  new HttpClient().get('http://241fcd3e.ngrok.io/api/resources', function(response) {
+    console.log('Response', response);
+    data = JSON.parse(response);
 
-			for (i = 0; i < texas_diaster_grid_size; i++) {
-				for (j = 0; j < texas_diaster_grid_size; j++) {
-			  		addBlock(j, i, texas_diaster_grid[i][j], "16513354751");
-			 	}
-			}
-// 		}	            
-//  	});
+    var x, y;
+
+    // This is the logic to normalize all the data and group it up
+    for (var i = 0; i < data.length; i++) {
+      // ya, my b on this equation!
+      x = Math.abs(parseInt(texas_diaster_grid_size * ((data[i].lng - TEXAS_DIASTER_DATA.ORIGIN.lng) / texas_diaster_normalize_w)));
+      y = Math.abs(parseInt(texas_diaster_grid_size * ((data[i].lat - TEXAS_DIASTER_DATA.ORIGIN.lat) / texas_diaster_normalize_h)));
+
+      if (data[i].supplies.indexOf("rescue") >= 0) {
+        texas_diaster_grid[x][y].rescue++;
+      }
+      if (data[i].supplies.indexOf("food") >= 0) {
+        texas_diaster_grid[x][y].food++;
+      }
+      if (data[i].supplies.indexOf("medical") >= 0) {
+        texas_diaster_grid[x][y].medical++;
+      }
+      if (data[i].supplies.indexOf("water") >= 0) {
+        texas_diaster_grid[x][y].water++;
+      }
+    }
+
+    for (i = 0; i < texas_diaster_grid_size; i++) {
+      for (j = 0; j < texas_diaster_grid_size; j++) {
+        addBlock(j, i, texas_diaster_grid[i][j], "16513354751");
+      }
+    }
+  });
 }
 
-/* 
+/*
 {
 	lat : Number, // between 0 - Grid_size
 	lng : Number, // between 0 - Grid_size
@@ -154,4 +145,18 @@ function resizeTexasDiasterMap() {
 	 	}
 	}
 
+}
+
+
+var HttpClient = function() {
+  this.get = function(aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function() {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.responseText);
+    }
+
+    anHttpRequest.open( "GET", aUrl, true );
+    anHttpRequest.send( null );
+  }
 }

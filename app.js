161dbcc5ea6fd61c6909dcef0a9c4ca1cb14d1a0
Noah Lebovic <noah@lebovic.com>
2017-10-28 19:36:52 -0500

    Added socket (with old spencer logic

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var dgram = require('dgram');
+var ip = require('ip');
+var io = require('socket.io')(server);
+var server = require('http').createServer(app); //creates an HTTP server instance
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -42,7 +46,6 @@ app.use('/', index);
 app.use('/users', users);
 // app.use('/api', api);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -60,5 +63,47 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// UDP socket for hardware
+var PORT = 6419;
+var HOST = ip.address();
+
+var dragonServer = dgram.createSocket('udp4');
+
+dragonServer.on('listening', function () {
+    var address = dragonServer.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+dragonServer.on('message', function (message, remote) {
+
+   var trimStr = message.toString().trim();
+
+   if (trimStr[0] == '0') {
+    // swtich colors
+    var isOn = parseInt(trimStr.substring(2,trimStr.length));
+    if (isOn) {
+      // only send socket when turned on
+      io.emit('nextColor',{});
+    }
+
+   } else if (trimStr[0] == '1') {
+    // light change
+    // console.log(trimStr.substring(2,trimStr.length));
+    var lightLevel = parseFloat(trimStr.substring(2,trimStr.length)) * 2.5;
+
+    // console.log("----" + lightLevel);
+    lightLevel = Math.max(0, lightLevel);
+    lightLevel = Math.min(100, lightLevel);
+    lightLevel = Math.floor(lightLevel);
+
+ console.log("Light Level: [" + lightLevel + " of 100]");
+
+    io.emit('lightLevel',{"level": lightLevel});
+   }
+
+});
+
+dragonServer.bind(PORT, HOST);
 
 module.exports = app;
